6dd7f6449c3d60f167e2d49eefc94e8660fce446
Bipin <bipp2009@gmail.com>
2021-03-26 17:25:52 +0000

    added each time block for the working day

---
+++ script.js
@@ -0,0 +1,6 @@
+// add on click to all save buttons - it needs to be dynamic - we need to be able to use one piece of code that works for all buttons
+//the button will add the value of the textarea (its sibling) to local storage with its parent the divs id a s an identifier
+
+// we need to use moment.js to display the current time on the screen
+
+//on page load we also want to check local storage for any previously saved values
