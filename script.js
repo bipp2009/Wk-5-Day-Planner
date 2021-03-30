7628dda6ab995c14bb995f0a318ac50a681000e4
Bipin <bipp2009@gmail.com>
2021-03-30 19:08:02 +0100

    Changed the colour of the blocks by comparing current time to past/present/future

--- script.js
+++ script.js
@@ -1,51 +1,56 @@
-// add on click to all save buttons - it needs to be dynamic - we need to be able to use one piece of code that works for all buttons
-//the button will add the value of the textarea (its sibling) to local storage with its parent the divs id a s an identifier
-$(".saveBtn").on("click", function(){
-    var diaryEntry = $(this).siblings(".description").val();
-    var diaryEntryTime = $(this).parent().attr('id');
-    localStorage.setItem(diaryEntryTime, diaryEntry)
-})
+$(".saveBtn").on("click", function () {
+  var diaryEntry = $(this).siblings(".description").val();
+  var diaryEntryTime = $(this).parent().attr("id");
+  localStorage.setItem(diaryEntryTime, diaryEntry);
+});
 
-// we need to use moment.js to display the current time on the screen - when page loads - get the html with an id of currentDay and set its value as the current time. Moment can be used to get the time. 
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY');
-    var timeDisplayEl = $('#currentDay');
-    timeDisplayEl.text(rightNow);
+  var rightNow = moment().format("MMM DD, YYYY");
+  var timeDisplayEl = $("#currentDay");
+  timeDisplayEl.text(rightNow);
+
+  $(".time-block").each(function () {
+    var activityHour = $(this).attr("id");
+    var currentHour = moment().hours();
+    
+    if (activityHour < currentHour) {
+      $(this).addClass("past");
+    } else if (activityHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
 }
-//we need to go into the html and loop over the time-blocks and use moment to get the current hour, if this is block id is less than current hour then apply 'past' class
-//if block hour and current hour are the same then apply present class
-//if block hour is greater than current then apply the future class
 
+var eightActivity = localStorage.getItem("8");
+$("#8 .description").val(eightActivity);
 
-//on page load we also want to check local storage for any previously saved values
-var eightActivity = localStorage.getItem('8');
-$("#8 .description").val(eightActivity); 
+var nineActivity = localStorage.getItem("9");
+$("#9 .description").val(nineActivity);
 
-var nineActivity = localStorage.getItem('9');
-$("#9 .description").val(nineActivity); 
+var tenActivity = localStorage.getItem("10");
+$("#10 .description").val(tenActivity);
 
-var tenActivity = localStorage.getItem('10');
-$("#10 .description").val(tenActivity); 
+var elevenActivity = localStorage.getItem("11");
+$("#11 .description").val(elevenActivity);
 
-var elevenActivity = localStorage.getItem('11');
-$("#11 .description").val(elevenActivity); 
+var twelveActivity = localStorage.getItem("12");
+$("#12 .description").val(twelveActivity);
 
-var twelveActivity = localStorage.getItem('12');
-$("#12 .description").val(twelveActivity); 
+var thirteenActivity = localStorage.getItem("13");
+$("#13 .description").val(thirteenActivity);
 
-var thirteenActivity = localStorage.getItem('13');
-$("#13 .description").val(thirteenActivity); 
+var fourteenActivity = localStorage.getItem("14");
+$("#14 .description").val(fourteenActivity);
 
-var fourteenActivity = localStorage.getItem('14');
-$("#14 .description").val(fourteenActivity); 
+var fifteenActivity = localStorage.getItem("15");
+$("#15 .description").val(fifteenActivity);
 
-var fifteenActivity = localStorage.getItem('15');
-$("#15 .description").val(fifteenActivity); 
+var sixteenActivity = localStorage.getItem("16");
+$("#16 .description").val(sixteenActivity);
 
-var sixteenActivity = localStorage.getItem('16');
-$("#16 .description").val(sixteenActivity); 
+var seventeenActivity = localStorage.getItem("17");
+$("#17 .description").val(seventeenActivity);
 
-var seventeenActivity = localStorage.getItem('17');
-$("#17 .description").val(seventeenActivity); 
-
-displayTime()
+displayTime();
